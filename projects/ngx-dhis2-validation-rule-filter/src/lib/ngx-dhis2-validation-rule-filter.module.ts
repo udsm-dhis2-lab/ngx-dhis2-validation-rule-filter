9ef29d71a1bee0db508e56b1cc19f66c1a5127d4
Walter V. Faustine <waltervfaustine@gmail.com>
2019-05-15 15:55:08 +0300

    feat: added projectlibrary

---
+++ projects/ngx-dhis2-validation-rule-filter/src/lib/ngx-dhis2-validation-rule-filter.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { NgxDhis2ValidationRuleFilterComponent } from './ngx-dhis2-validation-rule-filter.component';
+
+@NgModule({
+  declarations: [NgxDhis2ValidationRuleFilterComponent],
+  imports: [
+  ],
+  exports: [NgxDhis2ValidationRuleFilterComponent]
+})
+export class NgxDhis2ValidationRuleFilterModule { }
