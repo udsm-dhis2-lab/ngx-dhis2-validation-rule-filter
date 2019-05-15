35f0a6dfb5f626e8a85a18389882ca0f6b6c2a46
Walter V. Faustine <waltervfaustine@gmail.com>
2019-05-15 11:22:47 +0300

    fix: added FormModule and CommonModule dependencies

--- projects/ngx-dhis2-validation-rule-filter/src/lib/ngx-dhis2-validation-rule-filter.module.ts
+++ projects/ngx-dhis2-validation-rule-filter/src/lib/ngx-dhis2-validation-rule-filter.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
-import { NgxDhis2ValidationRuleFilterComponent } from './ngx-dhis2-validation-rule-filter.component';
-import { ValidationRuleFilterComponent } from './containers/validation-rule-filter/validation-rule-filter.component';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { NgxDhis2ValidationRuleFilterComponent } from './containers/ngx-dhis2-validation-rule-filter/ngx-dhis2-validation-rule-filter.component';
 
 @NgModule({
-  declarations: [NgxDhis2ValidationRuleFilterComponent, ValidationRuleFilterComponent],
-  imports: [
-  ],
-  exports: [NgxDhis2ValidationRuleFilterComponent]
+    declarations : [
+        NgxDhis2ValidationRuleFilterComponent,
+    ],
+    imports      : [CommonModule, FormsModule],
+    exports      : [
+        NgxDhis2ValidationRuleFilterComponent,
+    ],
 })
-export class NgxDhis2ValidationRuleFilterModule { }
+export class NgxDhis2ValidationRuleFilterModule {}
