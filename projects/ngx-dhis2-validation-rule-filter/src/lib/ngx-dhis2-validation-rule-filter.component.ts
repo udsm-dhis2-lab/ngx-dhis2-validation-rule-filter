2ce89068bc9ba049a326805a042bab8b4bb43752
Walter V. Faustine <waltervfaustine@gmail.com>
2019-05-16 01:25:35 +0300

    feat: added support to select validation rule group

--- projects/ngx-dhis2-validation-rule-filter/src/lib/ngx-dhis2-validation-rule-filter.component.ts
+++ projects/ngx-dhis2-validation-rule-filter/src/lib/ngx-dhis2-validation-rule-filter.component.ts
@@ -1,83 +1,121 @@
 import { Component, OnInit } from '@angular/core';
+import * as _ from 'lodash';
 
 @Component({
-  selector: 'lib-ngx-dhis2-validation-rule-filter',
-  templateUrl: './ngx-dhis2-validation-rule-filter.component.html',
-  styleUrls: ['./ngx-dhis2-validation-rule-filter.component.css']
+    selector    : 'lib-ngx-dhis2-validation-rule-filter',
+    templateUrl : './ngx-dhis2-validation-rule-filter.component.html',
+    styleUrls   : [
+        './ngx-dhis2-validation-rule-filter.component.css',
+    ],
 })
 export class NgxDhis2ValidationRuleFilterComponent implements OnInit {
+    selectedValidationRuleGroups: Array<any> = [];
+    availableValidationRuleGroups: Array<any> = [];
 
-  validationRuleGroups: Array<any> = [];
+    constructor() {}
 
-  constructor() { }
+    ngOnInit() {
+        this.availableValidationRuleGroups = [
+            {
+                id          : 'odiGzVETtQQ',
+                displayName : 'ANC',
+            },
+            {
+                id          : 'MR53j62xSj5',
+                displayName : 'CECAP',
+            },
+            {
+                id          : 'G5e4Yyoildx',
+                displayName : 'CECAP_NEW',
+            },
+            {
+                id          : 'NdRWOSO2oGT',
+                displayName : 'Child Health',
+            },
+            {
+                id          : 'weV7AEET8ih',
+                displayName : 'Delivery',
+            },
+            {
+                id          : 'FUDlbmg7yaw',
+                displayName : 'DTC',
+            },
+            {
+                id          : 'Y3Va0UFwhSK',
+                displayName : 'Form: Cervical cancer screening and treatment',
+            },
+            {
+                id          : 'Y6MG1Twa4Op',
+                displayName : 'Form: HIV Care and treatment reporting form',
+            },
+            {
+                id          : 'mgIfzegPcO2',
+                displayName : 'Form: Huduma baada ya kujifungua',
+            },
+            {
+                id          : 'muDkDa2p42d',
+                displayName : 'Form: Kliniki ya wajawazito',
+            },
+            {
+                id          : 'umCpkVWXw4x',
+                displayName : 'Form: Kutoka Wodi Ya Wazazi',
+            },
+            {
+                id          : 'WgozGzAvXPG',
+                displayName : 'Form: Magonjwa Ya Kuhara',
+            },
+            {
+                id          : 'YGBzGbzEebS',
+                displayName : 'Form: National Malaria control program',
+            },
+            {
+                id          : 'eejYUs0EC05',
+                displayName : 'Form: PMTCT MC',
+            },
+            {
+                id          : 'kTxVH6RsNoc',
+                displayName : 'Form: PMTCT PCR',
+            },
+            {
+                id          : 'vCfBUpp0SZP',
+                displayName : 'Form: Ufuatiliaji wa Watoto',
+            },
+        ];
+    }
 
-  ngOnInit() {
-    this.validationRuleGroups = [
-      {
-        id: 'odiGzVETtQQ',
-        displayName: 'ANC'
-      },
-      {
-        'id': 'MR53j62xSj5',
-        'displayName': 'CECAP'
-      },
-      {
-        'id': 'G5e4Yyoildx',
-        'displayName': 'CECAP_NEW'
-      },
-      {
-        'id': 'NdRWOSO2oGT',
-        'displayName': 'Child Health'
-      },
-      {
-        'id': 'weV7AEET8ih',
-        'displayName': 'Delivery'
-      },
-      {
-        'id': 'FUDlbmg7yaw',
-        'displayName': 'DTC'
-      },
-      {
-        'id': 'Y3Va0UFwhSK',
-        'displayName': 'Form: Cervical cancer screening and treatment'
-      },
-      {
-        'id': 'Y6MG1Twa4Op',
-        'displayName': 'Form: HIV Care and treatment reporting form'
-      },
-      {
-        'id': 'mgIfzegPcO2',
-        'displayName': 'Form: Huduma baada ya kujifungua'
-      },
-      {
-        'id': 'muDkDa2p42d',
-        'displayName': 'Form: Kliniki ya wajawazito'
-      },
-      {
-        'id': 'umCpkVWXw4x',
-        'displayName': 'Form: Kutoka Wodi Ya Wazazi'
-      },
-      {
-        'id': 'WgozGzAvXPG',
-        'displayName': 'Form: Magonjwa Ya Kuhara'
-      },
-      {
-        'id': 'YGBzGbzEebS',
-        'displayName': 'Form: National Malaria control program'
-      },
-      {
-        'id': 'eejYUs0EC05',
-        'displayName': 'Form: PMTCT MC'
-      },
-      {
-        'id': 'kTxVH6RsNoc',
-        'displayName': 'Form: PMTCT PCR'
-      },
-      {
-        'id': 'vCfBUpp0SZP',
-        'displayName': 'Form: Ufuatiliaji wa Watoto'
-      }
-    ];
-  }
+    popValidationRuleGroup = (
+        validationGroupList: Array<any>,
+        validationGroup: any
+    ): any => {
+
+        if (!validationGroup) {
+          return validationGroupList;
+        }
+
+        const selectedValidationRuleIndex: any = _.findIndex(
+            validationGroupList,
+            (validationRuleGroupItem) =>
+                validationRuleGroupItem.id === validationGroup.id
+        );
+
+        return selectedValidationRuleIndex !== -1 ? [
+          ...validationGroupList.slice(0, selectedValidationRuleIndex),
+          ...validationGroupList.slice(selectedValidationRuleIndex + 1),
+        ] : validationGroupList;
+    }
+
+    onSelectValidationRuleGroup = (validationRuleGroup: any, e) => {
+        e.stopPropagation();
+
+        this.selectedValidationRuleGroups = [
+            ...this.selectedValidationRuleGroups,
+            validationRuleGroup,
+        ];
+
+        this.availableValidationRuleGroups = this.popValidationRuleGroup(
+            this.availableValidationRuleGroups,
+            validationRuleGroup
+        );
+    }
 
 }
