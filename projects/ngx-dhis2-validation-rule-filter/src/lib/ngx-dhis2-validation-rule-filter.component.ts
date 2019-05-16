966c27c18cbe414e4bc25b3a6e5ad1dc69e51a25
Walter V. Faustine <waltervfaustine@gmail.com>
2019-05-16 15:34:57 +0300

    fix: removed static available validation rule

--- projects/ngx-dhis2-validation-rule-filter/src/lib/ngx-dhis2-validation-rule-filter.component.ts
+++ projects/ngx-dhis2-validation-rule-filter/src/lib/ngx-dhis2-validation-rule-filter.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import * as _ from 'lodash';
+import { NgxDhis2ValidationRuleFilterService } from './ngx-dhis2-validation-rule-filter.service';
 
 @Component({
     selector    : 'lib-ngx-dhis2-validation-rule-filter',
@@ -15,75 +16,12 @@ export class NgxDhis2ValidationRuleFilterComponent implements OnInit {
     @Output() update = new EventEmitter();
     @Output() close = new EventEmitter();
 
-    constructor() {}
+    constructor(private service: NgxDhis2ValidationRuleFilterService) {}
 
     ngOnInit() {
-        this.availableValidationRuleGroups = [
-            {
-                id          : 'odiGzVETtQQ',
-                displayName : 'ANC',
-            },
-            {
-                id          : 'MR53j62xSj5',
-                displayName : 'CECAP',
-            },
-            {
-                id          : 'G5e4Yyoildx',
-                displayName : 'CECAP_NEW',
-            },
-            {
-                id          : 'NdRWOSO2oGT',
-                displayName : 'Child Health',
-            },
-            {
-                id          : 'weV7AEET8ih',
-                displayName : 'Delivery',
-            },
-            {
-                id          : 'FUDlbmg7yaw',
-                displayName : 'DTC',
-            },
-            {
-                id          : 'Y3Va0UFwhSK',
-                displayName : 'Form: Cervical cancer screening and treatment',
-            },
-            {
-                id          : 'Y6MG1Twa4Op',
-                displayName : 'Form: HIV Care and treatment reporting form',
-            },
-            {
-                id          : 'mgIfzegPcO2',
-                displayName : 'Form: Huduma baada ya kujifungua',
-            },
-            {
-                id          : 'muDkDa2p42d',
-                displayName : 'Form: Kliniki ya wajawazito',
-            },
-            {
-                id          : 'umCpkVWXw4x',
-                displayName : 'Form: Kutoka Wodi Ya Wazazi',
-            },
-            {
-                id          : 'WgozGzAvXPG',
-                displayName : 'Form: Magonjwa Ya Kuhara',
-            },
-            {
-                id          : 'YGBzGbzEebS',
-                displayName : 'Form: National Malaria control program',
-            },
-            {
-                id          : 'eejYUs0EC05',
-                displayName : 'Form: PMTCT MC',
-            },
-            {
-                id          : 'kTxVH6RsNoc',
-                displayName : 'Form: PMTCT PCR',
-            },
-            {
-                id          : 'vCfBUpp0SZP',
-                displayName : 'Form: Ufuatiliaji wa Watoto',
-            },
-        ];
+        this.service.getValidationRuleGroups().subscribe((res) => {
+            res ? this.availableValidationRuleGroups = res.validationRuleGroups : this.availableValidationRuleGroups = [];
+        });
     }
 
     popValidationRuleGroup = (
