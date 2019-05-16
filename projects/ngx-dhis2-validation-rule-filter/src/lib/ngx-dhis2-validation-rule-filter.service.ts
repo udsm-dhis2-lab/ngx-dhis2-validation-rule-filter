159a94640f10ad482faa3de74d69219c973fdcac
Walter V. Faustine <waltervfaustine@gmail.com>
2019-05-16 15:37:48 +0300

    feat: added support toload validation rule group

--- projects/ngx-dhis2-validation-rule-filter/src/lib/ngx-dhis2-validation-rule-filter.service.ts
+++ projects/ngx-dhis2-validation-rule-filter/src/lib/ngx-dhis2-validation-rule-filter.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { NgxDhis2HttpClientService } from '@hisptz/ngx-dhis2-http-client';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NgxDhis2ValidationRuleFilterService {
 
-  constructor() { }
+  constructor(private httpClient: NgxDhis2HttpClientService) { }
+
+  getValidationRuleGroups = (): Observable<any> => {
+      return this.httpClient.get('validationRuleGroups.json');
+  }
 }
