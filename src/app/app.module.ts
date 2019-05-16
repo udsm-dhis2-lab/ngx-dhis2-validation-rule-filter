9700d4078a672797abf051c4ef96ee8c5fad0aa3
Walter V. Faustine <waltervfaustine@gmail.com>
2019-05-16 16:58:41 +0300

    fix: store support added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,36 @@ import { AppComponent } from './app.component';
 import { NgxDhis2ValidationRuleFilterModule } from 'projects/ngx-dhis2-validation-rule-filter/src/public-api';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 
+import { StoreModule, MetaReducer } from '@ngrx/store';
+import { EffectsModule, Actions, Effect, ofType } from '@ngrx/effects';
+
+
+// TodO: Import the following dependencies
+// BrowserAnimationModule
+
+// Not used in production
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { storeFreeze } from 'ngrx-store-freeze';
+
+
+// This would be done dynamically with webpack for builds
+const environment = {
+  development: true,
+  production: false,
+};
+
+export const metaReducers: MetaReducer<any>[] = !environment.production ? [storeFreeze] : [];
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BrowserModule,
+    // StoreModule.forRoot({ count: counterReducer })
+    // EffectsModule.forRoot([]),
+    // environment.development ? StoreDevtoolsModule.instrumentStore() : [],
   ],
   imports: [
     BrowserModule,
