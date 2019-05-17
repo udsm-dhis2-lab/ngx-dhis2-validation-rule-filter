39d3cc1a284afacf09ae19e79d7010ae14938b25
Walter V. Faustine <waltervfaustine@gmail.com>
2019-05-17 13:16:18 +0300

    fix: selector removed from reducer to selectors

--- projects/ngx-dhis2-validation-rule-filter/src/lib/store/reducers/index.ts
+++ projects/ngx-dhis2-validation-rule-filter/src/lib/store/reducers/index.ts
@@ -8,27 +8,3 @@ export interface ApplicationState {
 export const reducers: ActionReducerMap<ApplicationState> = {
     validationRuleGroups : fromReducer.reducer,
 };
-
-export const getApplicationState = createFeatureSelector<ApplicationState>(
-    'validationRuleGroups'
-);
-
-// Validation rule state
-export const getValidationRuleGroupState = createSelector(
-    getApplicationState,
-    (state: ApplicationState) => state.validationRuleGroups
-);
-
-
-export const getValidationRuleGroupsEntities = createSelector(getValidationRuleGroupState, fromReducer.getValidationRuleGroupsEntities);
-
-export const getAllValidationRuleGroups = createSelector(
-    getValidationRuleGroupsEntities,
-    (entities) => {
-        return Object.keys(entities).map(id => entities[id]);
-    }
-);
-
-export const getValidationRuleGroupsLoaded = createSelector(getValidationRuleGroupState, fromReducer.getValidationRuleGroupLoaded);
-export const getValidationRuleGroupsLoading = createSelector(getValidationRuleGroupState, fromReducer.getValidationRuleGroupLoading);
-
