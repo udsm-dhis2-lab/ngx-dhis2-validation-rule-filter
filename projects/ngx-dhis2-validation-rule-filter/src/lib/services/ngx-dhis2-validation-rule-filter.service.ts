40169940d1528556999c5b781213360726ba73f8
Walter V. Faustine <waltervfaustine@gmail.com>
2019-05-21 17:51:19 +0300

    fix(service): add support to load filtered validations

--- projects/ngx-dhis2-validation-rule-filter/src/lib/services/ngx-dhis2-validation-rule-filter.service.ts
+++ projects/ngx-dhis2-validation-rule-filter/src/lib/services/ngx-dhis2-validation-rule-filter.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { NgxDhis2HttpClientService } from '@hisptz/ngx-dhis2-http-client';
 import { Observable } from 'rxjs';
+import * as fromHelper from '../helpers';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,7 @@ export class NgxDhis2ValidationRuleFilterService {
 
   constructor(private httpClient: NgxDhis2HttpClientService) { }
 
-  getValidationRuleGroups = (): Observable<any> => {
-      return this.httpClient.get('validationRuleGroups.json');
+  getValidationRuleGroups = (payload: Array<string>): Observable<any> => {
+      return this.httpClient.get(fromHelper.generateValidationRuleAPI(payload));
   }
 }
