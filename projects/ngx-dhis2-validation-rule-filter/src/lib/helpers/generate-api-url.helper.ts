6ed223079f604112222260a1f2c996fa982c7148
Walter V. Faustine <waltervfaustine@gmail.com>
2019-05-21 17:32:50 +0300

    feat(helpers): added support to generate api url
    The support is added to generate dynamic url for the validation rule to be passed during the process
    of loading validation rule groups for a specific selected data element or indicator

---
+++ projects/ngx-dhis2-validation-rule-filter/src/lib/helpers/generate-api-url.helper.ts
@@ -0,0 +1,26 @@
+import * as _ from 'lodash';
+
+export function generateValidationRuleAPI (dataElementUID: string[]) {
+    const sanitizedDataElementUID = dataElementUID || [];
+    if (sanitizedDataElementUID.length !== 0) {
+        const validationRuleParameters = [
+            'id',
+            'name',
+            'periodType',
+        ];
+        const validationRuleGroupParameters = [
+            'id',
+            'name',
+        ];
+        const preURL = `validationRules.json?fields=[${validationRuleParameters.join(
+            ','
+        )}],validationRuleGroups[${validationRuleGroupParameters.join(',')}]&`;
+        const mapResult = _.map(sanitizedDataElementUID, (UID) => {
+            return `filter=leftSide.expression:ilike:${UID}&filter=rightSide.expression:ilike:${UID}`;
+        });
+        const postURL = `&rootJunction=OR`;
+        return preURL + mapResult.join('&') + postURL;
+    } else {
+        return 'validationRuleGroups.json';
+    }
+}
