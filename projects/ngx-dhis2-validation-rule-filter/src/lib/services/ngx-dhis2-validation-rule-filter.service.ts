1402b9af737191c2ef7bc021a9dcdef31453262a
Walter V. Faustine <waltervfaustine@gmail.com>
2019-06-05 12:45:46 +0300

    refactor(services): refactoring validation rule service

--- projects/ngx-dhis2-validation-rule-filter/src/lib/services/ngx-dhis2-validation-rule-filter.service.ts
+++ projects/ngx-dhis2-validation-rule-filter/src/lib/services/ngx-dhis2-validation-rule-filter.service.ts
@@ -1,16 +1,15 @@
 import { Injectable } from '@angular/core';
 import { NgxDhis2HttpClientService } from '@hisptz/ngx-dhis2-http-client';
 import { Observable } from 'rxjs';
-import * as fromHelper from '../helpers';
+import { generateValidationRuleGroupAPI } from '../helpers/generate-api-url.helper';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class NgxDhis2ValidationRuleFilterService {
-
   constructor(private httpClient: NgxDhis2HttpClientService) { }
 
   getValidationRuleGroups = (payload: Array<string>): Observable<any> => {
-      return this.httpClient.get(fromHelper.generateValidationRuleAPI(payload));
+    return this.httpClient.get(generateValidationRuleGroupAPI(payload));
   }
 }
