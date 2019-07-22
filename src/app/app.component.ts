8e9603f5e737fa53d82602e2f6e20810a6ab56b4
Walter V. Faustine <waltervfaustine@gmail.com>
2019-07-22 10:54:36 +0300

    fix: added support for least period type
    This commit will add the support to capture the least period type from the data selected in the
    selection filter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,7 +29,7 @@ export class AppComponent {
 
     this.selectedValidationRuleGroup = {
       items: [],
-      periodTypes: [],
+      periodType: [],
       dimension: {},
       changed: false
     }
